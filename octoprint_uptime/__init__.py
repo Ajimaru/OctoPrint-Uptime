d0c894fa49eccf497eaecd86a9c03db733b7974f
Ajimaru <38381946+Ajimaru@users.noreply.github.com>
2026-01-19 13:51:15 +0100

    chore: bump version to 0.1.0rc65

--- octoprint_uptime/__init__.py
+++ octoprint_uptime/__init__.py
@@ -507,4 +507,4 @@ __plugin_implementation__ = OctoprintUptimePlugin()
 __plugin_description__ = (
     "Adds system uptime to the navbar and exposes a small uptime API."
 )
-__plugin_version__ = "0.1.0rc64"
+__plugin_version__ = "0.1.0rc65"
