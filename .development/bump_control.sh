702defbdb3c0d8a682d2e9a152157e2fed1a96ff
Ajimaru <38381946+Ajimaru@users.noreply.github.com>
2026-01-26 00:26:30 +0100

    refactor: add option to run post-commit build script after version bump

--- .development/bump_control.sh
+++ .development/bump_control.sh
@@ -382,6 +382,24 @@ if [[ -z "$BUMP_TYPE" ]]; then
             git tag "$NEW_CURRENT"
         fi
         printf "%b\n" "$BUMP_TYPE bump completed."
+
+        if [[ -t 0 ]]; then
+            read -r -p "Run .development/post_commit_build_dist.sh now? [Y/n] " run_post
+            run_post=${run_post:-Y}
+        else
+            run_post=Y
+        fi
+        if [[ "$run_post" =~ ^[Yy] ]]; then
+            if [[ -x ".development/post_commit_build_dist.sh" ]]; then
+                if ! ./.development/post_commit_build_dist.sh; then
+                    printf "%b\n" "Warning: post_commit_build_dist.sh exited with an error." >&2
+                fi
+            else
+                if ! bash .development/post_commit_build_dist.sh; then
+                    printf "%b\n" "Warning: post_commit_build_dist.sh exited with an error." >&2
+                fi
+            fi
+        fi
         exit 0
     fi
     if [[ -n "$COMMIT" ]]; then
